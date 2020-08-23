269ad8f3bb98a5ef92ecd8cde3a916bcf3737433
ark4coding <ark4coding@gmail.com>
2020-08-23 12:52:04 +0100

    Done some changes for flake8.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -9,8 +9,8 @@ class ModelTests(TestCase):
         email = 'test@arkdev.com'
         password = 'Welcome123'
         user = get_user_model().objects.create_user(
-            email = email,
-            password = password
+            email=email,
+            password=password
         )
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
